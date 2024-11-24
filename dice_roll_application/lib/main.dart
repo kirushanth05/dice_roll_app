37c5e8869d7fe1b4c609ac913e76a0f6ea3852f7
kirushanth05 <kirushanthvijay@gmail.com>
2024-11-24 23:02:27 +0530

    Class HelloWorld renamed to DiceRollApp and the diceroll app UI created.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -2,17 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HelloWorldApp(),
+    home: DiceRollApp(),
   ));
 }
 
-class HelloWorldApp extends StatelessWidget {
-  const HelloWorldApp({super.key});
+class DiceRollApp extends StatelessWidget {
+  const DiceRollApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hello World!')),
+      backgroundColor: Colors.grey.shade200,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'DICE ROLL APP!',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black.withOpacity(0.41)),
+              ),
+              Image.asset(
+                'assets/images/dice-1.png',
+                height: 225,
+                width: 225,
+              ),
+              Container(
+                height: 51,
+                width: 298,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 29, 46, 29),
+                    borderRadius: BorderRadius.circular(12.0)),
+                child: Center(
+                    child: Text(
+                  'ROLL DICE',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                )),
+              ),
+            ]),
+      ),
     );
   }
 }
